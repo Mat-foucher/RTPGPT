c2fc4df873aff70dccc3fc7299ee3c11fb18cf5a
Mathieu Foucher <mfoucher@snowbird.com>
2025-07-19 09:21:19 -0600

    debug embedding- array flatten fix

--- app.py
+++ app.py
@@ -58,9 +58,9 @@ def ask_rtp_question(question, your_chunks,top_k=3, last_message = ""):
   try:
     question_embedding = model.encode(question)
   except Exception as e:
-    st.markdown('Embedding Error', e)
+    st.markdown('Embedding Error:', e)
   similarities = cosine_similarity([question_embedding], doc_embeddings)[0]
-  top_indices = similarities.argsort()[-top_k:][::-1]
+  top_indices = similarities.flatten().argsort()[-top_k:][::-1]
   top_chunks = [your_chunks[int(i)].page_content for i in top_indices]
 
   context = "\n\n".join(top_chunks)
