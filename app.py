b813422f8773657b458dd636eeafe3e1615c9748
Mathieu Foucher <mfoucher@snowbird.com>
2025-07-18 21:04:17 -0600

    debug fix for session memory

--- app.py
+++ app.py
@@ -59,8 +59,8 @@ def ask_rtp_question(question, your_chunks, doc_embedding, top_k=3, last_message
 
   context = "\n\n".join(top_chunks)
   if len(last_message) > 1:
-    st.markdown(last_message)
-    st.markdown(f"### last message: {type(last_message)}")
+    context = context + last_message['answer']
+    
   prompt = f"""You are a helpful and friendly assistant trained on Activeware's RTP documentation for ski resorts. 
   Nothing from the eStore documentation is to be brought up.
   Remember that new ticket types are organized under product headers. 
